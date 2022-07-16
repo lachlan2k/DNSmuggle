6721aca2f758e85955c254879bb47693b78a149a
Lachlan Davidson <lachlan@lachlandavidson.com>
2022-07-16 14:42:47 +1200

    ADding marshal methods to structs

--- internal/request/structs.go
+++ internal/request/structs.go
@@ -1,5 +1,11 @@
 package request
 
+import (
+	"bytes"
+	"errors"
+	"strconv"
+)
+
 const (
 	REQ_HEADER_SESSION_OPEN  = iota
 	REQ_HEADER_SESSION_POLL  = iota
@@ -7,32 +13,105 @@ const (
 	REQ_HEADER_SESSION_CLOSE = iota
 )
 
-// const (
-// 	RES_HEADER_WRITE_OK   = iota
-// 	RES_HEADER_POLL_OK    = iota
-// 	RES_HEADER_CLOSED     = iota
-// 	RES_HEADER_DIAL_ERROR = iota
-// )
+const (
+	RES_HEADER_WRITE_OK   = iota
+	RES_HEADER_POLL_OK    = iota
+	RES_HEADER_CLOSED     = iota
+	RES_HEADER_DIAL_ERROR = iota
+)
 
 type SessionID = uint64
 
-// type SessionOpenRequest struct {
-// 	DestAddr string
-// }
+func marshalSessionID(id SessionID) []byte {
+	return []byte(strconv.FormatUint(id, 10))
+}
 
-// type SessionOpenResponse struct {
-// 	ID SessionID
-// }
+func unmarshalSessionID(msg []byte) (id SessionID, err error) {
+	return strconv.ParseUint(string(msg), 10, 64)
+}
 
-// type PollRequest struct {
-// 	ID SessionID
-// }
+type Message interface {
+	Marshal() []byte
+	Unmarshal([]byte) error
+}
 
-// type SessionCloseRequest struct {
-// 	ID SessionID
-// }
+// Request to create a new session
+type SessionOpenRequest struct {
+	DestAddr string
+}
 
-// type WriteRequest struct {
-// 	ID   SessionID
-// 	Data []byte
-// }
+func (r *SessionOpenRequest) Unmarshal(msg []byte) error {
+	r.DestAddr = string(msg)
+	return nil
+}
+
+func (r SessionOpenRequest) Marshal() []byte {
+	var buff bytes.Buffer
+	buff.WriteByte(REQ_HEADER_SESSION_OPEN)
+	buff.WriteString(r.DestAddr)
+	return buff.Bytes()
+}
+
+type SessionOpenResponse struct {
+	ID SessionID
+}
+
+func (r *SessionOpenResponse) Unmarshal(msg []byte) (err error) {
+	r.ID, err = unmarshalSessionID(msg)
+	return
+}
+
+func (r SessionOpenResponse) Marshal() []byte {
+	return marshalSessionID(r.ID)
+}
+
+// Request to ask for new data
+type PollRequest struct {
+	ID SessionID
+}
+
+func (r *PollRequest) Unmarshal(msg []byte) (err error) {
+	r.ID, err = unmarshalSessionID(msg)
+	return
+}
+
+func (r PollRequest) Marshal() []byte {
+	return marshalSessionID(r.ID)
+}
+
+// Request to kill session
+type SessionCloseRequest struct {
+	ID SessionID
+}
+
+func (r *SessionCloseRequest) Unmarshal(msg []byte) (err error) {
+	r.ID, err = unmarshalSessionID(msg)
+	return
+}
+
+func (r SessionCloseRequest) Marshal() []byte {
+	return marshalSessionID(r.ID)
+}
+
+// Request to write data
+type WriteRequest struct {
+	ID   SessionID
+	Data []byte
+}
+
+func (r *WriteRequest) Unmarshal(msg []byte) (err error) {
+	if len(msg) < 8 {
+		return errors.New("Session write request too small")
+	}
+
+	r.ID, err = unmarshalSessionID(msg[:8])
+	r.Data = msg[8:]
+	return
+}
+
+func (r WriteRequest) Marshal() []byte {
+	var buff bytes.Buffer
+	buff.Write(marshalSessionID(r.ID))
+	buff.Write(r.Data)
+	return buff.Bytes()
+}
